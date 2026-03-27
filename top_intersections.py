b71d1f929db3709add5432a132f6af084fdf498a
Sebastian Guzman <sebastianguzman@MacBook-Pro-de-Sebastian-3.local>
2026-03-27 13:23:54 -0600

    fixed top intersection chart

--- top_intersections.py
+++ top_intersections.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
+print("Plot starting...")
 # Load CLEAN dataset
 df = pd.read_csv("Traffic_Incidents_filtered_2020.csv")
 
@@ -21,17 +23,18 @@ top_locations = top_locations[::-1]
 
 # Highlight top 2
 colors = ["lightgrey"] * len(top_locations)
-colors[-1] = "darkorange"
-colors[-2] = "darkorange"
+colors[-1] = "red"
+colors[-2] = "red"
 
 plt.figure(figsize=(11,6))
 
 plt.barh(top_locations.index, top_locations.values, color=colors)
 
-plt.title("Top Traffic Incident Locations (2021–2026)")
+plt.title("Top Traffic Incident Locations (2021-2026)")
 plt.xlabel("Number of Incidents")
 plt.ylabel("Location")
-
+sns.despine()
+plt.grid(axis="x", linestyle="--", alpha=0.3)
 plt.tight_layout()
 plt.savefig("top_intersections.png", dpi=300)
 
